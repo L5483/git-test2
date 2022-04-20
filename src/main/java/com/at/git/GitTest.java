f44fcdfeffd1af8dc01c27cb8564bb62e3956793
tete <t995483@163.com>
2022-04-20 15:30:42 +0800

    mas commit3

--- src/main/java/com/at/git/GitTest.java
+++ src/main/java/com/at/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("master-ver-1");
         System.out.println("master-ver-2");
+        System.out.println("master-ver-3");
     }
 }
